941bcbbfe5f7615ae47284a701c4a80a24dde86d
Dan O'Meara <danomearawd@gmail.com>
2024-10-24 02:41:11 -0400

    update

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,11 +1,16 @@
 # Import the dependencies.
-
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///../../Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
 
